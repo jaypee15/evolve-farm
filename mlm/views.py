bd87e6e53db81234ba6abff836f4000cbcd661cf
jaypee15 <okoyejohnpaul15@gmail.com>
2023-11-27 23:25:58 +0100

    deploy started

--- mlm/views.py
+++ mlm/views.py
@@ -1,17 +1,17 @@
-# from django.http import JsonResponse
-# from .model_handler import model
-# import json
+from django.http import JsonResponse
+from .model_handler import model
+import json
 
-# def predict_view(request):
-#     if request.method == 'POST':
-#         data = json.loads(request.body)
-#         input_data = data.get('data', None)
+def predict_view(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        input_data = data.get('data', None)
 
-#         if input_data:
-#             # Use the loaded model to make predictions
-#             prediction = model.predict(input_data)
-#             return JsonResponse({'prediction': prediction.tolist()})
-#         else:
-#             return JsonResponse({'error': 'Invalid input data'}, status=400)
-#     else:
-#         return JsonResponse({'error': 'Invalid request method'}, status=400)
+        if input_data:
+            # Use the loaded model to make predictions
+            prediction = model.predict(input_data)
+            return JsonResponse({'prediction': prediction.tolist()})
+        else:
+            return JsonResponse({'error': 'Invalid input data'}, status=400)
+    else:
+        return JsonResponse({'error': 'Invalid request method'}, status=400)
