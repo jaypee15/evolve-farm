8a6369a0df6ae5f4f08dc8fc014a348b130224e7
jaypee15 <okoyejohnpaul15@gmail.com>
2023-11-28 00:42:35 +0100

    fix: Add drfspectacular to apps

--- evolve/settings.py
+++ evolve/settings.py
@@ -19,10 +19,10 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', '0').lower() in ['true', 't', '1']
 
-# ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(' ')
-ALLOWED_HOSTS = []
-SECRET_KEY =  "SECRET"
-DEBUG = True
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(' ')
+# ALLOWED_HOSTS = []
+# SECRET_KEY =  "SECRET"
+# DEBUG = True
 
 
 
